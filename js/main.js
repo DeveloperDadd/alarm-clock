d9fb5763dc4c6785365f37e9f10e14de373212c4
Clayton Critchfield <clayton.critchfield@gmail.com>
2023-06-01 19:29:40 +0000

    fixed function to pad variable with 0s if less than 10

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
-function addZero(i) {
+function format(i) {
   if (i < 10) {
-    i = "0" + i; 
+    i = "0" + i;
+    return i;
+  } else {
     return i;
   }
 }
 
 function updateTime() {
   let time = new Date();
-  let hour = time.getHours();
-  let min = time.getMinutes();
-  let sec = time.getSeconds();
+  let hour = format(time.getHours());
+  let min = format(time.getMinutes());
+  let sec = format(time.getSeconds());
   document.getElementById("time").innerHTML = hour + ":" + min + ":" + sec;
 }
 
 setInterval(updateTime, 1000);
 
+alarmTime = setTime()
