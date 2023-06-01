68cc84e3a35af38bccb885969dda05a00d1c9c83
Clayton Critchfield <clayton.critchfield@gmail.com>
2023-06-01 19:03:07 +0000

    small editing changes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-
 function updateTime() {
-let time = new Date();
-let hour = time.getHours();
-let min = time.getMinutes();
-let sec = time.getSeconds();
-document.getElementById("time").innerHTML = hour + ":" + min + ":" + sec;
-setTimeout(updateTime, 1000);
+  let time = new Date();
+  let hour = time.getHours();
+  let min = time.getMinutes();
+  let sec = time.getSeconds();
+  document.getElementById("time").innerHTML = hour + ":" + min + ":" + sec;
+  setTimeout(updateTime, 1000);
 }
