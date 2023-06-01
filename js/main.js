90903ba24c837e02273436b38f9f83bef727f2f0
Clayton Critchfield <clayton.critchfield@gmail.com>
2023-06-01 20:58:54 +0000

    declared a new variable and edited some syntax

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//This function adds zeros to anything under 10
 function format(i) {
   if (i < 10) {
     i = "0" + i;
@@ -7,6 +8,7 @@ function format(i) {
   }
 }
 
+//this function retrieves the time in hours, minutes, and seconds and displays them in the DOM
 function updateTime() {
   let time = new Date();
   let hour = format(time.getHours());
@@ -15,6 +17,7 @@ function updateTime() {
   document.getElementById("time").innerHTML = hour + ":" + min + ":" + sec;
 }
 
+//This function updates the time every second
 setInterval(updateTime, 1000);
 
 alarmTime = setTime()
