96d152fb7c58446035486771e1f7ebbe865e6d23
Clayton Critchfield <clayton.critchfield@gmail.com>
2023-06-01 16:37:48 +0000

    added initial JS to retrieve current hour/minute/seconds from Date constructor

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
+let currentTime = new Date();
+let hour = currentTime.getHours();
+let minute = currentTime.getMinutes();
+let seconds = currentTime.getSeconds();
 
+
+let time = document.getElementById("time").innerHTML = hour + ":" + minute + ":" + seconds;
