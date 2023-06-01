46984b28da18da2497dda30732eba42eb67f745e
Clayton Critchfield <clayton.critchfield@gmail.com>
2023-06-01 18:36:33 +0000

    added a function to check time every second and display it in the innerHTML

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
-let currentTime = new Date();
-let hour = currentTime.getHours();
-let minute = currentTime.getMinutes();
-let seconds = currentTime.getSeconds();
+function updateTime () {
+  let currentTime = new Date();
+  let hour = currentTime.getHours();
+  let minute = currentTime.getMinutes();
+  let seconds = currentTime.getSeconds();
+  hour = formatTime(hour);
+  minute = formatTime(minute);
+  seconds = formatTime(seconds);
+  let time = document.getElementById("time").innerHTML = hour + ":" + minute + ":" + seconds;
+  setInterval(updateTime, 1000);
+};
 
 
-let time = document.getElementById("time").innerHTML = hour + ":" + minute + ":" + seconds;
+function formatTime(time) {
+    if (time < 10) {
+        return "0" + time;
+    }
+}
