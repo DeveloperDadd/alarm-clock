ae1197a39d8e3e085da0cc7bfe4eb47280bdd9d8
Clayton Critchfield <clayton.critchfield@gmail.com>
2023-06-02 02:49:32 +0000

    Added an array for months to display when accessed in the array

--- js/main.js
+++ js/main.js
@@ -25,9 +25,48 @@ function updateTime() {
     alert("DON'T IGNORE ME, YOU SET THIS ALARM");
   }
 }
+// create an array of all the months
+const months = ["January", "February", "March", "April", "May", "June", "July",
+"August", "September", "October", "November", "December"];
+
+function updateDate() {
+  let today = new Date();
+  let day = today.getDay();
+  let month = today.getMonth();
+  if (month === 0) {
+    month = "January";
+  } else if (month === 1) {
+    month = "February";
+  } else if (month === 2) {
+    month = "March";
+  } else if (month === 3) {
+    month = "April";
+  } else if (month === 4) {
+    month = "May";
+  } else if (month === 5) {
+    month = "June";
+  } else if (month === 6) {
+    month = "July";
+  } else if (month === 7) {
+    month = "August";
+  } else if (month === 8) {
+    month = "September";
+  } else if (month === 9) {
+    month = "October";
+  } else if (month === 10) {
+    month = "November";
+  } else if (month === 11) {
+    month = "December";
+  }
+  let year = today.getFullYear();
+  let todaysDate = month + " " + day + ", " + year;
+  console.log(todaysDate);
+  document.getElementById("date").innerHTML = todaysDate;
+}
 
 //This function updates the time every second
 setInterval(updateTime, 1000);
+setInterval(updateDate, 1000);
 
 //This function adds zeros to anything under 10
 function format(i) {
